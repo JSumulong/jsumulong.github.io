edc91203777ccee484dbd40b403bd308a94f0a85
Jsumulong <sumulong28@gmail.com>
2017-03-05 21:54:16 -0600

    add media queries to container-four

--- src/ContainerFour.js
+++ src/ContainerFour.js
@@ -5,7 +5,7 @@ class ContainerFour extends Component {
 	render() {
 		return (
 			<div className="container-four">
-				<h1><a href="https://github.com/JSumulong">GitHub</a> | <a href="https://www.linkedin.com/in/jsumulong/">LinkedIn</a></h1>
+				<h1 className="h1-media"><a className="a-media" href="https://github.com/JSumulong">GitHub</a> | <a className="a-media" href="https://www.linkedin.com/in/jsumulong/">LinkedIn</a></h1>
 			</div>
 		)
 	}
