720204f4fa94645e17a4e7f048ebcc249d2078d5
Jsumulong <sumulong28@gmail.com>
2017-04-30 11:35:24 -0500

    add javascript and eventlisteners for new 'Projects' section

--- javascript.js
+++ javascript.js
@@ -1,10 +1,38 @@
 const resume = document.querySelector('.resume-selector');
+const vacationer = document.querySelector('.vacationer-selector');
+const congo = document.querySelector('.congo-selector');
+const remindr = document.querySelector('.remindr-selector');
+
+function toggleProjectsOff() {
+	let projectDisplays = document.querySelectorAll('.project-display-wrapper');
+	projectDisplays.forEach(project => project.classList.remove('active'));
+}
+
 function toggleResumeProject() {
 	var resumeDisplay = document.querySelector('.resume-display');
-	if (document.querySelector('.resume-display.active')) {
-		resumeDisplay.classList.remove('active');
-	} else {
-		resumeDisplay.classList.add('active');
-	}
+	toggleProjectsOff();
+	resumeDisplay.classList.add('active');
 }
+
+function toggleVacationerProject() {
+	var vacationerDisplay = document.querySelector('.vacationer-display');
+	toggleProjectsOff();
+	vacationerDisplay.classList.add('active');
+}
+
+function toggleCongoProject() {
+	var congoDisplay = document.querySelector('.congo-display');
+	toggleProjectsOff();
+	congoDisplay.classList.add('active');
+}
+
+function toggleRemindrProject() {
+	var remindrDisplay = document.querySelector('.remindr-display');
+	toggleProjectsOff();
+	remindrDisplay.classList.add('active');
+}
+
 resume.addEventListener('click', toggleResumeProject);
+vacationer.addEventListener('click', toggleVacationerProject);
+congo.addEventListener('click', toggleCongoProject);
+remindr.addEventListener('click', toggleRemindrProject);
